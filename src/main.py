7e8275f0405dcfca168652c613f462031330e1ca
Nicholas Rawitscher <nicholas.rawitscher@hammermissions.com>
2022-03-30 22:47:11 +0100

    updates

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
-import socket
-import threading
+
 from tello import Tello
 
 tello = Tello()
 tello.connect()
+tello.listen_to_tello()
 tello.run_test()
--- src/tello.py
+++ src/tello.py
@@ -9,21 +9,37 @@ class Tello:
     def __init__(self):
         self.host = ''
         self.port = 9000
-        self.locaddr = (self.host,self.port)
+        self.client_machine = (self.host,self.port)
         self.trello_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.log =''
         self.address = ('192.168.10.1', 8889)
+        self.receiving_thread = None
+        self.can_run = True
 
     def connect(self):
         try:
-            self.trello_socket.bind(self.locaddr)
-
-            skd_state = "Command"
+            self.trello_socket.bind(self.client_machine)
+            skd_state = "command"
             skd_state = skd_state.encode(encoding="utf-8") 
             self.log = self.trello_socket.sendto(skd_state, self.address)
             print('connected')
-        except:
-            print(self.log)
+        except socket.error as e:
+            print("Error creating socket: %s" % e)
+    
+    def listen_to_tello(self):
+        self.receiving_thread = threading.Thread(target = self.__receive_from_tello)
+        self.receiving_thread.start()
+
+    def __receive_from_tello(self):
+        count = 0
+        while True: 
+            try:
+                data, server = self.trello_socket.recvfrom(1518)
+                print("Trello says: %s" % data.decode(encoding="utf-8"))
+            except Exception:
+                print ("An error occured while hearing back from Trello ):")
+                self.can_run = False
+                break
 
     # Terminates the connection with the Drone.
     def disconnect(self):
@@ -42,10 +58,9 @@ class Tello:
             command = "takeoff"
             command = command.encode(encoding="utf-8") 
             self.log = self.log = self.trello_socket.sendto(command, self.address)
-            print("taking off")
-            print(self.log)
-        except:
-            print("failed")
+            
+        except socket.error as e:
+            print("Error taking off: %s" % e)
 
     # Moves the drone x cm's to the right.
     # "x" should be between 20 - 500 cm
@@ -64,29 +79,44 @@ class Tello:
         command = 'forward ' + str(x)
         command = command.encode(encoding="utf-8") 
         self.log = self.trello_socket.sendto(command, self.address)
+        # print("Moved forward by: %s" % x +" cm")
+        print(command)
+
+    def move_back(self,x):
+        command = 'back ' + str(x)
+        command = command.encode(encoding="utf-8") 
+        self.log = self.trello_socket.sendto(command, self.address)
 
     def run_test(self):
-        while True: 
-
-            # end -- quit demo
+        while self.can_run: 
             try:
                 msg = input("")
+                if not msg:break  
 
-                print(msg) 
-
-                if not msg:
-                    break  
+                # if(msg =='W'):
+                #     self.move_forward(80)
+                # if(msg == 'S'):
+                #     self.move_back(50)
+                # if(msg == 'T'):
+                #     self.take_off()
+                # if(msg == 'L'):
+                #     self.land()
 
                 if 'end' in msg:
-                    print ('...')
-                    self.trello_socket.close()  
+                    self.disconnect()
+                    print ('Tello disconnected')
                     break
 
                 # Send data
                 msg = msg.encode(encoding="utf-8") 
                 sent = self.trello_socket.sendto(msg, self.address)
-            except KeyboardInterrupt:
-                print ('\n . . .\n')
-                self.trello_socket.close()  
+                # print("Command sent to Trello: %s" % sent)
+            # except KeyboardInterrupt:
+            #     print ('\n . . .\n')
+            #     self.disconnect()   
+            #     break
+            except:
+                self.disconnect() 
                 break
+        if(self.can_run is False): self.disconnect() 
 
