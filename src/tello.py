bc07b4129f0c737f1600f588dca046c8f7e96541
Nicholas Rawitscher <nicholas.rawitscher@gmail.com>
2022-04-02 00:22:41 +0100

    Added better handling for sending control commands to Tello

--- src/tello.py
+++ src/tello.py
@@ -2,142 +2,182 @@
 from dis import Instruction
 import threading 
 import socket
+import time
+import logging
 
-# 
+
 class Tello:
 
-    def __init__(self):
-        self.host = ''
-        self.port = 9000
-        self.client_machine = (self.host,self.port)
-        self.trello_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    TELLO_IP_ADRESS ='192.168.10.1'
+    TELLO_PORT_NUM = 8889
+    COMMAND_RETRY_COUNT = 10
+    RESPONSE_TIMEOUT = 7 #seconds
+    MAX_RETRY_COMMAND_COUNT = 3
+
+    def __init__(self, client_machine_ip: str = ''):
+       
+        # self.port = 9000
+        self.address = (Tello.TELLO_IP_ADRESS, Tello.TELLO_PORT_NUM)
+        self.client_machine = (client_machine_ip,Tello.TELLO_PORT_NUM) #(self.client_machine_ip,self.port)
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.log =''
-        self.address = ('192.168.10.1', 8889)
-        self.receiving_thread = None
+        self.receive_response_thread = None
         self.can_run = True
         self.counter = 0
+        self.last_command_received_at = time.time()
+        self.tello_responses =[]
+
 
     def connect(self):
         try:
-            self.trello_socket.bind(self.client_machine)
+            self.client_socket.bind(self.client_machine)
             skd_state = "command"
             skd_state = skd_state.encode(encoding="utf-8") 
-            self.log = self.trello_socket.sendto(skd_state, self.address)
+            self.log = self.client_socket.sendto(skd_state, self.address)
         except socket.error as e:
             print("Error creating socket: %s" % e)
     
     def listen_to_tello(self):
-        self.receiving_thread = threading.Thread(target = self.__receive_from_tello)
-        self.receiving_thread.start()
+        self.receive_response_thread = threading.Thread(target = self.__receive_response_from_tello)
+        self.receive_response_thread.start()
 
-    def __receive_from_tello(self):
-        count = 0
-        while self.can_run: 
+    def __receive_response_from_tello(self):
+        while True: 
             try:
-                data, server = self.trello_socket.recvfrom(1518)
-                message_from_trello = data.decode(encoding="utf-8")
-                print("Trello says: %s" % message_from_trello)
-                isBattery = message_from_trello.isnumeric()
-                if(isBattery):
-                    if(int(message_from_trello) <=15):
-                        print("Trello says: My battery is below 15%, I will start landing...")
-                        self.land()
+                data, address = self.client_socket.recvfrom(1518)
+                address = address[0]
+                print('Data received from {} at client_socket'.format(address))
+                if(address!=self.address): continue
+                self.tello_responses.append(data)
             except Exception:
                 print ("An error occured while hearing back from Trello ):")
-                self.can_run = False
                 break
 
-    def __send_command(self,command):
-        command = command.encode(encoding="utf-8") 
-        self.trello_socket.sendto(command, self.address)
+    def __try_send_control_command(self, command:str) -> str:
+
+        delta_time = time.time() - self.last_command_received_at
+        if delta_time < 0.1:
+            time.sleep(delta_time)
+
+        self.client_socket.sendto(command.encode('utf-8'), self.address)
+
+        current_time = time.time()
+       
+        # If there are no responses, will check time out, if time
+        # has exeeded, this method will abort.
+        while not self.tello_responses:
+            # aborts command if did not receive response back from Tello
+            # after response timeout
+            if time.time() - current_time > Tello.RESPONSE_TIMEOUT:
+                error_message = "Aborting command '{}'. Did not receive a response after the timeout of {} seconds".format(command, Tello.RESPONSE_TIMEOUT)
+                print( error_message)
+                return error_message
+
+            time.sleep(0.1)
+
+        self.last_command_received_at = time.time()
+
+        # returns latest response.
+        first_response = self.tello_responses.pop(0)
+        try:
+            response = first_response.decode("utf-8")
+        except UnicodeDecodeError as e:
+            error_message = "Encountered an error while decoding response:{}".format(e)
+            return error_message
+
+        response = response.rstrip("\r\n")
+        print("Response from command {}: '{}'".format(command, response))
+        return response
+
+    # Send a command which controls the behavior of Tello
+    def send_action_command(self, command:str):
+        response = "max retries exceeded"
+        for i in range(0, Tello.MAX_RETRY_COMMAND_COUNT): # 3 = rmax etry count
+            response = self.__try_send_control_command(command)
+            if 'ok' in response.lower():return
+
+            print("Command attempt #{} failed for command: '{}'".format(i, command))
+
+        print("Max retries for command {} have exceeded".format(command))
 
 
     # Terminates the connection with the Drone.
     def disconnect(self):
-        self.trello_socket.close()
-        self.can_run = False
+        self.client_socket.close()
     
     # Drone auto- takes off.
     def take_off(self):
-        try:
-            command = "takeoff"
-            # self.__send_command(self,command)
-            command = command.encode(encoding="utf-8") 
-            self.trello_socket.sendto(command, self.address)
-            
-        except socket.error as e:
-            print("Error taking off: %s" % e)
+        self.send_action_command('takeoff')
 
       # Lands the drone in the ground.
     def land(self):
-        command = 'land'
-        command = command.encode(encoding="utf-8") 
-        self.log = self.trello_socket.sendto(command, self.address)
+        self.send_action_command('land')
 
     # Moves the drone x cm's to the right.
     # "x" should be between 20 - 500 cm
     def move_right(self,x):
         command = 'left ' + str(x)
         command = command.encode(encoding="utf-8") 
-        self.log = self.trello_socket.sendto(command, self.address)
+        self.log = self.client_socket.sendto(command, self.address)
 
     # Moves the drone x cm's to the left.
     # "x" should be between 20 - 500 cm.
     def move_left(self,x):
         command = 'left ' + str(x)
         command = command.encode(encoding="utf-8") 
-        self.trello_socket.sendto(command, self.address)
+        self.client_socket.sendto(command, self.address)
 
      # Moves the drone x cm's forward.
      # "x" should be between 20 - 500 cm.
     def move_forward(self,x):
         command = 'forward ' + str(x)
         command = command.encode(encoding="utf-8") 
-        self.log = self.trello_socket.sendto(command, self.address)
+        self.log = self.client_socket.sendto(command, self.address)
 
     def move_back(self,x):
         command = 'back ' + str(x)
         command = command.encode(encoding="utf-8") 
-        self.log = self.trello_socket.sendto(command, self.address)
+        self.log = self.client_socket.sendto(command, self.address)
 
     def move_up(self,x):
         command = 'up ' + str(x)
         command = command.encode(encoding="utf-8") 
-        self.log = self.trello_socket.sendto(command, self.address)
+        self.log = self.client_socket.sendto(command, self.address)
     
     def move_down(self,x):
         command = 'down ' + str(x)
         command = command.encode(encoding="utf-8") 
-        self.log = self.trello_socket.sendto(command, self.address)
+        self.log = self.client_socket.sendto(command, self.address)
     
     def move_left(self,x):
         command = 'left ' + str(x)
         command = command.encode(encoding="utf-8") 
-        self.log = self.trello_socket.sendto(command, self.address)
+        self.log = self.client_socket.sendto(command, self.address)
 
     def move_right(self,x):
         command = 'right ' + str(x)
         command = command.encode(encoding="utf-8") 
-        self.log = self.trello_socket.sendto(command, self.address)
+        self.log = self.client_socket.sendto(command, self.address)
 
     def rotate(self,x):
         command = 'cw ' + str(x)
         command = command.encode(encoding="utf-8") 
-        self.log = self.trello_socket.sendto(command, self.address)
+        self.log = self.client_socket.sendto(command, self.address)
 
     def get_life(self):
         command = 'battery?'
         command = command.encode(encoding="utf-8") 
-        self.log = self.trello_socket.sendto(command, self.address)
+        self.log = self.client_socket.sendto(command, self.address)
 
     def start_video_stream(self):
           command = 'streamon'
           command = command.encode(encoding="utf-8") 
-          self.log = self.trello_socket.sendto(command, self.address)
+          self.log = self.client_socket.sendto(command, self.address)
 
 
     def run_test(self):
-        while self.can_run: 
+        while True: 
             try:
                 intruction = input("")
                 if not input:break  
@@ -182,5 +222,5 @@ class Tello:
             except:
                 self.disconnect() 
                 break
-        if(self.can_run is False): self.disconnect() 
+       
 
